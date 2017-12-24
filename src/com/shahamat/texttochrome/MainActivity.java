6f7b4505d9706a8d7b3ec2636265556760b83f97
shasha1334 <shane.shahamat@gmail.com>
2017-12-23 21:13:10 -0600

    Initial

--- src/com/shahamat/texttochrome/MainActivity.java
+++ src/com/shahamat/texttochrome/MainActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.casthelloworld;
+package com.shahamat.texttochrome;
 
 import com.google.android.gms.cast.ApplicationMetadata;
 import com.google.android.gms.cast.Cast;
